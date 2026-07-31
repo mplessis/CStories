87a5bc7c331c8d730df367764a0e4a541d44fb63
Marc PLESSIS <marc.plessis@legrand.com>
2026-07-31 13:55:30 +0200

    refactor(sample): apply the CStories Gradle plugin instead of manual wiring

--- sample/build.gradle.kts
+++ sample/build.gradle.kts
@@ -1,31 +1,16 @@
-import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
-
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
     alias(libs.plugins.compose.multiplatform)
     alias(libs.plugins.compose.compiler)
-    alias(libs.plugins.ksp)
+    id("io.cstories.gradle")
 }
 
-@OptIn(ExperimentalWasmDsl::class)
 kotlin {
     jvm()
-    wasmJs {
-        browser()
-    }
 
     sourceSets {
         commonMain.dependencies {
-            implementation(project(":cstories-annotations"))
-            implementation(project(":cstories-runtime"))
             implementation(compose.material3)
         }
     }
-}
-
-dependencies {
-    // Keep the sample wired manually inside this monorepo. The Gradle plugin
-    // is validated independently and will be exercised from a consumer-style
-    // build once repository-local plugin resolution is set up.
-    add("kspCommonMainMetadata", project(":cstories-processor"))
 }
