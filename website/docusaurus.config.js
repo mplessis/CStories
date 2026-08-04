d6685af0af506f9ec1f01eaeedcae4bfb624655c
Marc PLESSIS <mplessis@hotmail.com>
2026-08-04 13:22:15 +0200

    doc(*): change base url of docusaurus

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   url: 'https://www.cstories.dev',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/CStories/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   organizationName: 'mplessis',
